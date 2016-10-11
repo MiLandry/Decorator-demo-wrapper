6dcb0d2f5f563f86edbfb6795d80f0ea1a87f15f
mland_laptop <mland@DESKTOP-PNEL2RP>
2016-10-11 15:54:11 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+its java,
+
+
+clone this into eclipse
+
+
+run as : java app
+
---
+++ src/hamsterofdark/Main.java
@@ -0,0 +1,11 @@
+package hamsterofdark;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("decorator");
+		// TODO Auto-generated method stub
+
+	}
+
+}
